7032994e265214fb174d73225c519cb749556374
Renato Dias <renatocardosodias@icloud.com>
2023-08-31 22:53:51 -0300

    Iniciando Projeto

---
+++ AppTodo/AppTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppTodoApp.swift
+//  AppTodo
+//
+//  Created by Renato on 31/08/23.
+//
+
+import SwiftUI
+
+@main
+struct AppTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
